4a530da60c25845fb9e843d098b395cdaeb065eb
Fleacon <david.noskov0603@gmail.com>
2025-07-29 02:38:53 +0200

    Fix memory address mapping in Bus read/write methods

--- GameboyEmulator/Bus.cs
+++ GameboyEmulator/Bus.cs
@@ -19,16 +19,16 @@ public class Bus
         HRAM = new byte[126];
     }
 
-    public void Write(ushort addr, byte data)
+    public void Write8(ushort addr, byte data)
     {
         if (addr < 0x7FFF) 
             return;
         if (addr is >= 0x8000 and <= 0x9FFF)
-            VRAM[addr % 8192] = data;
+            VRAM[addr - 0x8000] = data;
         if (addr is >= 0xC000 and <= 0xDFFF)
-            WRAM[addr % 8192] = data;
+            WRAM[addr - 0xC000] = data;
         if (addr is >= 0xFF80 and <= 0xFFFE)
-            HRAM[addr % 126] = data;
+            HRAM[addr - 0xFF80] = data;
         if (addr is 0xFFFF)
             IERegister = data;
     }
@@ -38,13 +38,13 @@ public class Bus
         if (addr <= 0x3FFF)
             return ROMBank00[addr % 16384];
         if (addr is >= 0x4000 and <= 0x7FFF)
-            return ROMBankNN[addr % 16384];
+            return ROMBankNN[addr - 0x4000];
         if (addr is >= 0x8000 and <= 0x9FFF)
-            return VRAM[addr % 8192];
+            return VRAM[addr - 0x8000];
         if (addr is >= 0xC000 and <= 0xDFFF)
-            return WRAM[addr % 8192];
+            return WRAM[addr - 0xC000];
         if (addr is >= 0xFF80 and <= 0xFFFE)
-            return HRAM[addr % 126];
+            return HRAM[addr - 0xFF80];
         if (addr is 0xFFFF)
             return IERegister;
         return 0x0000;

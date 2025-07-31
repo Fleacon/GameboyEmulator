a059c21937e0cf6633803c2c052b517829c2e6a9
Fleacon <david.noskov0603@gmail.com>
2025-07-31 20:23:56 +0200

    Refactor Instruction struct to use CyclesBranch

--- GameboyEmulator/CPU/Instruction.cs
+++ GameboyEmulator/CPU/Instruction.cs
@@ -1,10 +1,10 @@
 namespace GameboyEmulator;
 
-public struct Instruction(string name, Action operate, Func<ushort> addressingMode, byte cycles, bool isRelative = false)
+public struct Instruction(string name, Action operate, Func<ushort> addressingMode, byte cycles, byte cyclesBranch = 0)
 {
     public readonly string Name = name;
     public readonly Action Typ = operate;
     public readonly Func<ushort> AddressingMode = addressingMode;
     public readonly byte Cycles = cycles;
-    public readonly bool isRelative;
+    public readonly byte CyclesBranch = cyclesBranch;
 }

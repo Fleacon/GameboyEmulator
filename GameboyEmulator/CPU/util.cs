9debbf6d724b19a210d4e5a00ac6b06b895b4de2
Fleacon <david.noskov0603@gmail.com>
2025-07-31 20:23:35 +0200

    Add utility methods for opcode bitmasking

---
+++ GameboyEmulator/CPU/util.cs
@@ -0,0 +1,20 @@
+using System.Numerics;
+using static GameboyEmulator.LR35902;
+
+namespace GameboyEmulator;
+
+public static class util
+{
+    public static byte ReadCode(byte opCode, byte mask)
+    {
+        var shift = BitOperations.TrailingZeroCount(mask);
+        return (byte)((opCode & mask) >> shift);
+    }
+    
+    public static byte ReadCode(byte opCode, Masks mask)
+    {
+        var m = (byte)mask;
+        var shift = BitOperations.TrailingZeroCount(m);
+        return (byte)((opCode & m) >> shift);
+    }
+}

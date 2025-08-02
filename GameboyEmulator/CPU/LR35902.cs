8466785186e04ddacdf311240420f0d1dc633cd7
Fleacon <david.noskov0603@gmail.com>
2025-08-02 02:34:24 +0200

    Add instruction initialization for main opcode array
    Implemented the initInstructionArray() method to populate the main instruction array with all standard Game Boy CPU opcodes and their metadata. Also added a stub for initCBInstructionArray(). This sets up the instruction decoding infrastructure for the emulator.

--- GameboyEmulator/CPU/LR35902.cs
+++ GameboyEmulator/CPU/LR35902.cs
@@ -874,5 +874,302 @@ public class LR35902
         var newVal = (byte)((0x01 << index) | val);
         
         Registers.SetR8(code, newVal);
+        
+        isCB = false;
+    }
+
+    private void initInstructionArray()
+    {
+        instructions[0x00] = new("NOP", NOP, IMP, 1);
+        instructions[0x01] = new("LD BC, u16", LD_r16, fetchIMM16, 3);
+        instructions[0x02] = new("LD (BC), A", LD_r16mem, fetchR16MEM, 2);
+        instructions[0x03] = new("INC BC", INC_R16, fetchR16, 2);
+        instructions[0x04] = new("INC B", INC_R8, fetchR8, 1);
+        instructions[0x05] = new("DEC B", DEC_R8, fetchR8, 1);
+        instructions[0x06] = new("LD B, u8", LD_r8, fetchIMM8, 2);
+        instructions[0x07] = new("RLCA", RLCA, IMP, 1);
+        instructions[0x08] = new("LD (u16), SP", LD_imm16mem_SP, fetchIMM16, 5);
+        instructions[0x09] = new("ADD HL, BC", ADD_HL, fetchR16, 2);
+        instructions[0x0A] = new("LD A, (BC)", LD_a, fetchR16MEM, 2);
+        instructions[0x0B] = new("DEC BC", DEC_R16, fetchR16, 2);
+        instructions[0x0C] = new("INC C", INC_R8, fetchR8, 1);
+        instructions[0x0D] = new("DEC C", DEC_R8, fetchR8, 1);
+        instructions[0x0E] = new("LD C, u8", LD_r8, fetchIMM8, 2);
+        instructions[0x0F] = new("RRCA", RRCA, IMP, 1);
+
+        // 0x10-0x1F
+        instructions[0x10] = new("STOP", STOP, fetchIMM8, 1);
+        instructions[0x11] = new("LD DE, u16", LD_r16, fetchIMM16, 3);
+        instructions[0x12] = new("LD (DE), A", LD_r16mem, fetchR16MEM, 2);
+        instructions[0x13] = new("INC DE", INC_R16, fetchR16, 2);
+        instructions[0x14] = new("INC D", INC_R8, fetchR8, 1);
+        instructions[0x15] = new("DEC D", DEC_R8, fetchR8, 1);
+        instructions[0x16] = new("LD D, u8", LD_r8, fetchIMM8, 2);
+        instructions[0x17] = new("RLA", RLA, IMP, 1);
+        instructions[0x18] = new("JR i8", JR, fetchIMM8, 3);
+        instructions[0x19] = new("ADD HL, DE", ADD_HL, fetchR16, 2);
+        instructions[0x1A] = new("LD A, (DE)", LD_a, fetchR16MEM, 2);
+        instructions[0x1B] = new("DEC DE", DEC_R16, fetchR16, 2);
+        instructions[0x1C] = new("INC E", INC_R8, fetchR8, 1);
+        instructions[0x1D] = new("DEC E", DEC_R8, fetchR8, 1);
+        instructions[0x1E] = new("LD E, u8", LD_r8, fetchIMM8, 2);
+        instructions[0x1F] = new("RRA", RRA, IMP, 1);
+
+        // 0x20-0x2F
+        instructions[0x20] = new("JR NZ, i8", JR_cond, fetchIMM8, 2, 3);
+        instructions[0x21] = new("LD HL, u16", LD_r16, fetchIMM16, 3);
+        instructions[0x22] = new("LD (HL+), A", LD_r16mem, fetchR16MEM, 2);
+        instructions[0x23] = new("INC HL", INC_R16, fetchR16, 2);
+        instructions[0x24] = new("INC H", INC_R8, fetchR8, 1);
+        instructions[0x25] = new("DEC H", DEC_R8, fetchR8, 1);
+        instructions[0x26] = new("LD H, u8", LD_r8, fetchIMM8, 2);
+        instructions[0x27] = new("DAA", DAA, IMP, 1);
+        instructions[0x28] = new("JR Z, i8", JR_cond, fetchIMM8, 2, 3);
+        instructions[0x29] = new("ADD HL, HL", ADD_HL, fetchR16, 2);
+        instructions[0x2A] = new("LD A, (HL+)", LD_a, fetchR16MEM, 2);
+        instructions[0x2B] = new("DEC HL", DEC_R16, fetchR16, 2);
+        instructions[0x2C] = new("INC L", INC_R8, fetchR8, 1);
+        instructions[0x2D] = new("DEC L", DEC_R8, fetchR8, 1);
+        instructions[0x2E] = new("LD L, u8", LD_r8, fetchIMM8, 2);
+        instructions[0x2F] = new("CPL", CPL, IMP, 1);
+
+        // 0x30-0x3F
+        instructions[0x30] = new("JR NC, i8", JR_cond, fetchIMM8, 2, 3);
+        instructions[0x31] = new("LD SP, u16", LD_r16, fetchIMM16, 3);
+        instructions[0x32] = new("LD (HL-), A", LD_r16mem, fetchR16MEM, 2);
+        instructions[0x33] = new("INC SP", INC_R16, fetchR16, 2);
+        instructions[0x34] = new("INC (HL)", INC_R8, fetchR8, 3);
+        instructions[0x35] = new("DEC (HL)", DEC_HLmem, IMP, 3);
+        instructions[0x36] = new("LD (HL), u8", LD_r8, fetchIMM8, 3);
+        instructions[0x37] = new("SCF", SCF, IMP, 1);
+        instructions[0x38] = new("JR C, i8", JR_cond, fetchIMM8, 2, 3);
+        instructions[0x39] = new("ADD HL, SP", ADD_HL, fetchR16, 2);
+        instructions[0x3A] = new("LD A, (HL-)", LD_a, fetchR16MEM, 2);
+        instructions[0x3B] = new("DEC SP", DEC_R16, fetchR16, 2);
+        instructions[0x3C] = new("INC A", INC_R8, fetchR8, 1);
+        instructions[0x3D] = new("DEC A", DEC_R8, fetchR8, 1);
+        instructions[0x3E] = new("LD A, u8", LD_r8, fetchIMM8, 2);
+        instructions[0x3F] = new("CCF", CCF, IMP, 1);
+
+        // 0x40-0x4F (LD r8, r8 instructions)
+        instructions[0x40] = new("LD B, B", LD_r8, fetchR8, 1);
+        instructions[0x41] = new("LD B, C", LD_r8, fetchR8, 1);
+        instructions[0x42] = new("LD B, D", LD_r8, fetchR8, 1);
+        instructions[0x43] = new("LD B, E", LD_r8, fetchR8, 1);
+        instructions[0x44] = new("LD B, H", LD_r8, fetchR8, 1);
+        instructions[0x45] = new("LD B, L", LD_r8, fetchR8, 1);
+        instructions[0x46] = new("LD B, (HL)", LD_r8, fetchR8, 2);
+        instructions[0x47] = new("LD B, A", LD_r8, fetchR8, 1);
+        instructions[0x48] = new("LD C, B", LD_r8, fetchR8, 1);
+        instructions[0x49] = new("LD C, C", LD_r8, fetchR8, 1);
+        instructions[0x4A] = new("LD C, D", LD_r8, fetchR8, 1);
+        instructions[0x4B] = new("LD C, E", LD_r8, fetchR8, 1);
+        instructions[0x4C] = new("LD C, H", LD_r8, fetchR8, 1);
+        instructions[0x4D] = new("LD C, L", LD_r8, fetchR8, 1);
+        instructions[0x4E] = new("LD C, (HL)", LD_r8, fetchR8, 2);
+        instructions[0x4F] = new("LD C, A", LD_r8, fetchR8, 1);
+
+        // 0x50-0x5F
+        instructions[0x50] = new("LD D, B", LD_r8, fetchR8, 1);
+        instructions[0x51] = new("LD D, C", LD_r8, fetchR8, 1);
+        instructions[0x52] = new("LD D, D", LD_r8, fetchR8, 1);
+        instructions[0x53] = new("LD D, E", LD_r8, fetchR8, 1);
+        instructions[0x54] = new("LD D, H", LD_r8, fetchR8, 1);
+        instructions[0x55] = new("LD D, L", LD_r8, fetchR8, 1);
+        instructions[0x56] = new("LD D, (HL)", LD_r8, fetchR8, 2);
+        instructions[0x57] = new("LD D, A", LD_r8, fetchR8, 1);
+        instructions[0x58] = new("LD E, B", LD_r8, fetchR8, 1);
+        instructions[0x59] = new("LD E, C", LD_r8, fetchR8, 1);
+        instructions[0x5A] = new("LD E, D", LD_r8, fetchR8, 1);
+        instructions[0x5B] = new("LD E, E", LD_r8, fetchR8, 1);
+        instructions[0x5C] = new("LD E, H", LD_r8, fetchR8, 1);
+        instructions[0x5D] = new("LD E, L", LD_r8, fetchR8, 1);
+        instructions[0x5E] = new("LD E, (HL)", LD_r8, fetchR8, 2);
+        instructions[0x5F] = new("LD E, A", LD_r8, fetchR8, 1);
+
+        // 0x60-0x6F
+        instructions[0x60] = new("LD H, B", LD_r8, fetchR8, 1);
+        instructions[0x61] = new("LD H, C", LD_r8, fetchR8, 1);
+        instructions[0x62] = new("LD H, D", LD_r8, fetchR8, 1);
+        instructions[0x63] = new("LD H, E", LD_r8, fetchR8, 1);
+        instructions[0x64] = new("LD H, H", LD_r8, fetchR8, 1);
+        instructions[0x65] = new("LD H, L", LD_r8, fetchR8, 1);
+        instructions[0x66] = new("LD H, (HL)", LD_r8, fetchR8, 2);
+        instructions[0x67] = new("LD H, A", LD_r8, fetchR8, 1);
+        instructions[0x68] = new("LD L, B", LD_r8, fetchR8, 1);
+        instructions[0x69] = new("LD L, C", LD_r8, fetchR8, 1);
+        instructions[0x6A] = new("LD L, D", LD_r8, fetchR8, 1);
+        instructions[0x6B] = new("LD L, E", LD_r8, fetchR8, 1);
+        instructions[0x6C] = new("LD L, H", LD_r8, fetchR8, 1);
+        instructions[0x6D] = new("LD L, L", LD_r8, fetchR8, 1);
+        instructions[0x6E] = new("LD L, (HL)", LD_r8, fetchR8, 2);
+        instructions[0x6F] = new("LD L, A", LD_r8, fetchR8, 1);
+
+        // 0x70-0x7F
+        instructions[0x70] = new("LD (HL), B", LD_r8, fetchR8, 2);
+        instructions[0x71] = new("LD (HL), C", LD_r8, fetchR8, 2);
+        instructions[0x72] = new("LD (HL), D", LD_r8, fetchR8, 2);
+        instructions[0x73] = new("LD (HL), E", LD_r8, fetchR8, 2);
+        instructions[0x74] = new("LD (HL), H", LD_r8, fetchR8, 2);
+        instructions[0x75] = new("LD (HL), L", LD_r8, fetchR8, 2);
+        instructions[0x76] = new("HALT", HALT, IMP, 1);
+        instructions[0x77] = new("LD (HL), A", LD_r8, fetchR8, 2);
+        instructions[0x78] = new("LD A, B", LD_r8, fetchR8, 1);
+        instructions[0x79] = new("LD A, C", LD_r8, fetchR8, 1);
+        instructions[0x7A] = new("LD A, D", LD_r8, fetchR8, 1);
+        instructions[0x7B] = new("LD A, E", LD_r8, fetchR8, 1);
+        instructions[0x7C] = new("LD A, H", LD_r8, fetchR8, 1);
+        instructions[0x7D] = new("LD A, L", LD_r8, fetchR8, 1);
+        instructions[0x7E] = new("LD A, (HL)", LD_r8, fetchR8, 2);
+        instructions[0x7F] = new("LD A, A", LD_r8, fetchR8, 1);
+
+        // 0x80-0x8F (ADD, ADC)
+        instructions[0x80] = new("ADD A, B", ADD, fetchR8, 1);
+        instructions[0x81] = new("ADD A, C", ADD, fetchR8, 1);
+        instructions[0x82] = new("ADD A, D", ADD, fetchR8, 1);
+        instructions[0x83] = new("ADD A, E", ADD, fetchR8, 1);
+        instructions[0x84] = new("ADD A, H", ADD, fetchR8, 1);
+        instructions[0x85] = new("ADD A, L", ADD, fetchR8, 1);
+        instructions[0x86] = new("ADD A, (HL)", ADD, fetchR8, 2);
+        instructions[0x87] = new("ADD A, A", ADD, fetchR8, 1);
+        instructions[0x88] = new("ADC A, B", ADC, fetchR8, 1);
+        instructions[0x89] = new("ADC A, C", ADC, fetchR8, 1);
+        instructions[0x8A] = new("ADC A, D", ADC, fetchR8, 1);
+        instructions[0x8B] = new("ADC A, E", ADC, fetchR8, 1);
+        instructions[0x8C] = new("ADC A, H", ADC, fetchR8, 1);
+        instructions[0x8D] = new("ADC A, L", ADC, fetchR8, 1);
+        instructions[0x8E] = new("ADC A, (HL)", ADC, fetchR8, 2);
+        instructions[0x8F] = new("ADC A, A", ADC, fetchR8, 1);
+
+        // 0x90-0x9F (SUB, SBC)
+        instructions[0x90] = new("SUB A, B", SUB, fetchR8, 1);
+        instructions[0x91] = new("SUB A, C", SUB, fetchR8, 1);
+        instructions[0x92] = new("SUB A, D", SUB, fetchR8, 1);
+        instructions[0x93] = new("SUB A, E", SUB, fetchR8, 1);
+        instructions[0x94] = new("SUB A, H", SUB, fetchR8, 1);
+        instructions[0x95] = new("SUB A, L", SUB, fetchR8, 1);
+        instructions[0x96] = new("SUB A, (HL)", SUB, fetchR8, 2);
+        instructions[0x97] = new("SUB A, A", SUB, fetchR8, 1);
+        instructions[0x98] = new("SBC A, B", SBC, fetchR8, 1);
+        instructions[0x99] = new("SBC A, C", SBC, fetchR8, 1);
+        instructions[0x9A] = new("SBC A, D", SBC, fetchR8, 1);
+        instructions[0x9B] = new("SBC A, E", SBC, fetchR8, 1);
+        instructions[0x9C] = new("SBC A, H", SBC, fetchR8, 1);
+        instructions[0x9D] = new("SBC A, L", SBC, fetchR8, 1);
+        instructions[0x9E] = new("SBC A, (HL)", SBC, fetchR8, 2);
+        instructions[0x9F] = new("SBC A, A", SBC, fetchR8, 1);
+
+        // 0xA0-0xAF (AND, XOR)
+        instructions[0xA0] = new("AND A, B", AND, fetchR8, 1);
+        instructions[0xA1] = new("AND A, C", AND, fetchR8, 1);
+        instructions[0xA2] = new("AND A, D", AND, fetchR8, 1);
+        instructions[0xA3] = new("AND A, E", AND, fetchR8, 1);
+        instructions[0xA4] = new("AND A, H", AND, fetchR8, 1);
+        instructions[0xA5] = new("AND A, L", AND, fetchR8, 1);
+        instructions[0xA6] = new("AND A, (HL)", AND, fetchR8, 2);
+        instructions[0xA7] = new("AND A, A", AND, fetchR8, 1);
+        instructions[0xA8] = new("XOR A, B", XOR, fetchR8, 1);
+        instructions[0xA9] = new("XOR A, C", XOR, fetchR8, 1);
+        instructions[0xAA] = new("XOR A, D", XOR, fetchR8, 1);
+        instructions[0xAB] = new("XOR A, E", XOR, fetchR8, 1);
+        instructions[0xAC] = new("XOR A, H", XOR, fetchR8, 1);
+        instructions[0xAD] = new("XOR A, L", XOR, fetchR8, 1);
+        instructions[0xAE] = new("XOR A, (HL)", XOR, fetchR8, 2);
+        instructions[0xAF] = new("XOR A, A", XOR, fetchR8, 1);
+
+        // 0xB0-0xBF (OR, CP)
+        instructions[0xB0] = new("OR A, B", OR, fetchR8, 1);
+        instructions[0xB1] = new("OR A, C", OR, fetchR8, 1);
+        instructions[0xB2] = new("OR A, D", OR, fetchR8, 1);
+        instructions[0xB3] = new("OR A, E", OR, fetchR8, 1);
+        instructions[0xB4] = new("OR A, H", OR, fetchR8, 1);
+        instructions[0xB5] = new("OR A, L", OR, fetchR8, 1);
+        instructions[0xB6] = new("OR A, (HL)", OR, fetchR8, 2);
+        instructions[0xB7] = new("OR A, A", OR, fetchR8, 1);
+        instructions[0xB8] = new("CP A, B", CP, fetchR8, 1);
+        instructions[0xB9] = new("CP A, C", CP, fetchR8, 1);
+        instructions[0xBA] = new("CP A, D", CP, fetchR8, 1);
+        instructions[0xBB] = new("CP A, E", CP, fetchR8, 1);
+        instructions[0xBC] = new("CP A, H", CP, fetchR8, 1);
+        instructions[0xBD] = new("CP A, L", CP, fetchR8, 1);
+        instructions[0xBE] = new("CP A, (HL)", CP, fetchR8, 2);
+        instructions[0xBF] = new("CP A, A", CP, fetchR8, 1);
+
+        // 0xC0-0xCF
+        instructions[0xC0] = new("RET NZ", RET_cond, IMP, 2, 5);
+        instructions[0xC1] = new("POP BC", POP, IMP, 3);
+        instructions[0xC2] = new("JP NZ, u16", JP_cond, fetchIMM16, 3, 4);
+        instructions[0xC3] = new("JP u16", JP_imm16, fetchIMM16, 4);
+        instructions[0xC4] = new("CALL NZ, u16", CALL_cond, fetchIMM16, 3, 6);
+        instructions[0xC5] = new("PUSH BC", PUSH, fetchR16STK, 4);
+        instructions[0xC6] = new("ADD A, u8", ADD, fetchIMM8, 2);
+        instructions[0xC7] = new("RST 00h", RST, IMP, 4);
+        instructions[0xC8] = new("RET Z", RET_cond, IMP, 2, 5);
+        instructions[0xC9] = new("RET", RET, IMP, 4);
+        instructions[0xCA] = new("JP Z, u16", JP_cond, fetchIMM16, 3, 4);
+        instructions[0xCB] = new("PREFIX CB", PREFIX_CB, IMP, 1);
+        instructions[0xCC] = new("CALL Z, u16", CALL_cond, fetchIMM16, 3, 6);
+        instructions[0xCD] = new("CALL u16", CALL, fetchIMM16, 6);
+        instructions[0xCE] = new("ADC A, u8", ADC, fetchIMM8, 2);
+        instructions[0xCF] = new("RST 08h", RST, IMP, 4);
+
+        // 0xD0-0xDF
+        instructions[0xD0] = new("RET NC", RET_cond, IMP, 2, 5);
+        instructions[0xD1] = new("POP DE", POP, IMP, 3);
+        instructions[0xD2] = new("JP NC, u16", JP_cond, fetchIMM16, 3, 4);
+        instructions[0xD3] = new("INVALID", NOP, IMP, 1); // Invalid opcode
+        instructions[0xD4] = new("CALL NC, u16", CALL_cond, fetchIMM16, 3, 6);
+        instructions[0xD5] = new("PUSH DE", PUSH, fetchR16STK, 4);
+        instructions[0xD6] = new("SUB A, u8", SUB, fetchIMM8, 2);
+        instructions[0xD7] = new("RST 10h", RST, IMP, 4);
+        instructions[0xD8] = new("RET C", RET_cond, IMP, 2, 5);
+        instructions[0xD9] = new("RETI", RETI, IMP, 4);
+        instructions[0xDA] = new("JP C, u16", JP_cond, fetchIMM16, 3, 4);
+        instructions[0xDB] = new("INVALID", NOP, IMP, 1); // Invalid opcode
+        instructions[0xDC] = new("CALL C, u16", CALL_cond, fetchIMM16, 3, 6);
+        instructions[0xDD] = new("INVALID", NOP, IMP, 1); // Invalid opcode
+        instructions[0xDE] = new("SBC A, u8", SBC, fetchIMM8, 2);
+        instructions[0xDF] = new("RST 18h", RST, IMP, 4);
+
+        // 0xE0-0xEF
+        instructions[0xE0] = new("LDH (u8), A", LDH_imm8mem, fetchIMM8, 3);
+        instructions[0xE1] = new("POP HL", POP, IMP, 3);
+        instructions[0xE2] = new("LDH (C), A", LDH_cmem, IMP, 2);
+        instructions[0xE3] = new("INVALID", NOP, IMP, 1); // Invalid opcode
+        instructions[0xE4] = new("INVALID", NOP, IMP, 1); // Invalid opcode
+        instructions[0xE5] = new("PUSH HL", PUSH, fetchR16STK, 4);
+        instructions[0xE6] = new("AND A, u8", AND, fetchIMM8, 2);
+        instructions[0xE7] = new("RST 20h", RST, IMP, 4);
+        instructions[0xE8] = new("ADD SP, i8", ADD_SP, fetchIMM8, 4);
+        instructions[0xE9] = new("JP HL", JP_hl, IMP, 1);
+        instructions[0xEA] = new("LD (u16), A", LD_imm16mem_a, fetchIMM16, 4);
+        instructions[0xEB] = new("INVALID", NOP, IMP, 1); // Invalid opcode
+        instructions[0xEC] = new("INVALID", NOP, IMP, 1); // Invalid opcode
+        instructions[0xED] = new("INVALID", NOP, IMP, 1); // Invalid opcode
+        instructions[0xEE] = new("XOR A, u8", XOR, fetchIMM8, 2);
+        instructions[0xEF] = new("RST 28h", RST, IMP, 4);
+
+        // 0xF0-0xFF
+        instructions[0xF0] = new("LDH A, (u8)", LDH_a_imm8mem, fetchIMM8, 3);
+        instructions[0xF1] = new("POP AF", POP, IMP, 3);
+        instructions[0xF2] = new("LDH A, (C)", LDH_a_cmem, IMP, 2);
+        instructions[0xF3] = new("DI", DI, IMP, 1);
+        instructions[0xF4] = new("INVALID", NOP, IMP, 1); // Invalid opcode
+        instructions[0xF5] = new("PUSH AF", PUSH, fetchR16STK, 4);
+        instructions[0xF6] = new("OR A, u8", OR, fetchIMM8, 2);
+        instructions[0xF7] = new("RST 30h", RST, IMP, 4);
+        instructions[0xF8] = new("LD HL, SP+i8", LD_HL_SP_imm8, fetchIMM8, 3);
+        instructions[0xF9] = new("LD SP, HL", LD_SP_HL, IMP, 2);
+        instructions[0xFA] = new("LD A, (u16)", LD_a, fetchIMM16, 4);
+        instructions[0xFB] = new("EI", EI, IMP, 1);
+        instructions[0xFC] = new("INVALID", NOP, IMP, 1); // Invalid opcode
+        instructions[0xFD] = new("INVALID", NOP, IMP, 1); // Invalid opcode
+        instructions[0xFE] = new("CP A, u8", CP, fetchIMM8, 2);
+        instructions[0xFF] = new("RST 38h", RST, IMP, 4);
+    }
+    
+    private void initCBInstructionArray()
+    {
+        
     }
 }

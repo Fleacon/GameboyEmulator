28fb38502c2e7ead48c7e9b527b917c70ee882be
Fleacon <david.noskov0603@gmail.com>
2025-08-02 02:29:13 +0200

    Add ROM loading and IO register support to Bus

--- GameboyEmulator/Bus.cs
+++ GameboyEmulator/Bus.cs
@@ -7,6 +7,7 @@ public class Bus
     private byte[] VRAM;
     private byte[] WRAM;
     private byte[] EchoRAM; // prohibited
+    private IO IOregister;
     private byte[] HRAM;
     private byte IERegister;
 
@@ -16,7 +17,37 @@ public class Bus
         ROMBankNN = new byte[16384]; // 16 KiB
         VRAM = new byte[8192]; // 8 KiB
         WRAM = new byte[8192]; // 8 KiB
+        IOregister = new IO();
         HRAM = new byte[126];
+        
+        IERegister = 0x00;
+    }
+
+    public void LoadROM(string filePath)
+    {
+        if (!File.Exists(filePath))
+        {
+            throw new FileNotFoundException("File not found", filePath);
+        }
+        
+        byte[] rom = File.ReadAllBytes(filePath);
+
+        if (rom.Length > ROMBank00.Length + ROMBankNN.Length)
+        {
+            throw new IndexOutOfRangeException("ROM size is too large");
+        }
+        
+        int firstBankSize = Math.Min(rom.Length, ROMBank00.Length);
+        Array.Copy(rom, 0, ROMBank00, 0, firstBankSize);
+        
+        if (rom.Length > ROMBank00.Length)
+        {
+            int bytesRemaining = rom.Length - ROMBank00.Length;
+            
+            int secondBankSize = Math.Min(bytesRemaining, ROMBankNN.Length);
+            Array.Copy(rom, ROMBank00.Length, ROMBankNN, 0, secondBankSize);
+        }
+        Console.WriteLine("ROM loaded");
     }
 
     public void Write8(ushort addr, byte data)
@@ -27,6 +58,8 @@ public class Bus
             VRAM[addr - 0x8000] = data;
         if (addr is >= 0xC000 and <= 0xDFFF)
             WRAM[addr - 0xC000] = data;
+        if (addr is >= 0xFF00 and <= 0xFF7F)
+            IOregister.Write((ushort)(addr - 0xFF00), data);
         if (addr is >= 0xFF80 and <= 0xFFFE)
             HRAM[addr - 0xFF80] = data;
         if (addr is 0xFFFF)
@@ -52,10 +85,12 @@ public class Bus
             return VRAM[addr - 0x8000];
         if (addr is >= 0xC000 and <= 0xDFFF)
             return WRAM[addr - 0xC000];
+        if (addr is >= 0xFF00 and <= 0xFF7F)
+            return IOregister.Read((ushort)(addr - 0xFF00));
         if (addr is >= 0xFF80 and <= 0xFFFE)
             return HRAM[addr - 0xFF80];
         if (addr is 0xFFFF)
             return IERegister;
-        return 0x0000;
+        return 0;
     }
 }
